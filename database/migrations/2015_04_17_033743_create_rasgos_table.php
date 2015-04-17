56471c3a0ddb7b438e4a860be095266acc978d20
Julio Reyes <nemesiscodex@gmail.com>
2015-04-17 13:19:11 -0400

    Added a max, avg, min value for the rasgo model

--- database/migrations/2015_04_17_033743_create_rasgos_table.php
+++ database/migrations/2015_04_17_033743_create_rasgos_table.php
@@ -18,6 +18,9 @@ class CreateRasgosTable extends Migration {
 			$table->timestamps();
 			$table->string('name');
 			$table->longText('desc');
+			$table->integer('max')->default(6);
+			$table->integer('avg')->default(5);
+			$table->integer('min')->default(4);
 			$table->longText('max_desc');
 			$table->longText('avg_desc');
 			$table->longText('min_desc');
