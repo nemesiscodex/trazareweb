dc51ae197456ec1faa6a2eb65c50cdfbc9b112dd
Julio Reyes <nemesiscodex@gmail.com>
2015-04-17 13:23:07 -0400

    Create a new angular directive for items

---
+++ public/js/survey.surveyItem.directive.js
@@ -0,0 +1,23 @@
+(function(){
+  angular.module('survey')
+    .directive('surveyItem', function(){
+      return {
+        restrict: 'C',
+        template: '<div style="cursor: pointer"' +
+        ' class="centrado pregunta" ng-class="{active: item.selected}" '+
+          'onclick="$(this).find(\'input:checkbox\').click()">' +
+          '<img class="img-responsive center-block" width="205px" ng-src="/img/items/{{ item.img }}"' +
+          ' alt="{{ item.name }}" >' +
+          '<p>{{ item.name }}</p><br>' +
+          '<div class="checkbox-item">' +
+            	'<input type="checkbox"  style="cursor: inherit" ng-model="item.selected" onclick="event.stopPropagation()" value="None"' +
+               ' id="selected" name="selected" />' +
+          '</div>' +
+        '</div>',
+        scope: {
+          item: '=item'
+        }
+      };
+    })
+    ;
+})();
