200a6a67817892b2102b60825c5f6350ba52cace
Julio Reyes <nemesiscodex@gmail.com>
2015-04-16 23:17:53 -0400

    Config database

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'database'  => env('DB_DATABASE', 'trazare'),
+			'username'  => env('DB_USERNAME', 'trazare'),
+			'password'  => env('DB_PASSWORD', 'trazare'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
@@ -67,9 +67,9 @@ return [
 		'pgsql' => [
 			'driver'   => 'pgsql',
 			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
+			'database' => env('DB_DATABASE', 'trazare'),
+			'username' => env('DB_USERNAME', 'trazare'),
+			'password' => env('DB_PASSWORD', 'trazare'),
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
@@ -78,9 +78,9 @@ return [
 		'sqlsrv' => [
 			'driver'   => 'sqlsrv',
 			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
+			'database' => env('DB_DATABASE', 'trazare'),
+			'username' => env('DB_USERNAME', 'trazare'),
+			'password' => env('DB_PASSWORD', 'trazare'),
 			'prefix'   => '',
 		],
 
