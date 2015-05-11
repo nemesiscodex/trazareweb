7c711ef103b5d4aee58016983cb4770db36f1696
Julio Reyes <nemesiscodex@gmail.com>
2015-05-11 18:42:12 -0400

    Arregla errores en el path de imagenes, y layout

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -81,162 +81,162 @@ class RasgoItemTableSeeder extends Seeder {
 
 		Item::create([
 			'name' => 'Enseñar a otros a leer y escribir',
-			'img' => 'social/item1-1.jpg',
+			'img' => 'social/1-1.png',
 			'rasgo_id' => $rango1->id,
 			]);
 		Item::create([
 			'name' => 'Defender y discutir representando a otras personas, para llegar a la verdad',
-			'img' => 'social/item1-2.jpg',
+			'img' => 'social/1-2.png',
 			'rasgo_id' => $rango1->id,
 			]);
 		Item::create([
 			'name' => 'Relatar sucesos de una comunidad escribiendo noticias',
-			'img' => 'social/item1-3.jpg',
+			'img' => 'social/1-3.png',
 			'rasgo_id' => $rango1->id,
 			]);
 		Item::create([
 			'name' => 'Asistir a las personas carenciadas en momentos difíciles como inundaciones, viviendas, vestimentas, comida, etc.',
-			'img' => 'social/item1-4.jpg',
+			'img' => 'social/1-4.png',
 			'rasgo_id' => $rango1->id,
 			]);
 		Item::create([
 			'name' => 'Organizar las actividades del grupo de amigos',
-			'img' => 'social/item1-5.jpg',
+			'img' => 'social/1-5.png',
 			'rasgo_id' => $rango1->id,
 			]);
 
 		Item::create([
 			'name' => 'Compartir con personas solidarias, sociables y entusiastas la mayor parte del tiempo',
-			'img' => 'social/item1-6.jpg',
+			'img' => 'social/1-6.png',
 			'rasgo_id' => $rango1->id,
 			]);		
 
 		Item::create([
 			'name' => 'Sanar heridas, cuidar enfermos ',
-			'img' => 'salud/item2-1.jpg',
+			'img' => 'salud/2-1.png',
 			'rasgo_id' => $rango2->id,
 			]);
 		Item::create([
 			'name' => 'Cuidar de mi mascota y proteger todo ser vivo',
-			'img' => 'salud/item2-2jpg',
+			'img' => 'salud/2-2.png',
 			'rasgo_id' => $rango2->id,
 			]);
 		Item::create([
 			'name' => 'Conocer  la conducta de las personas o grupos ',
-			'img' => 'salud/item2-3.jpg',
+			'img' => 'salud/2-3.png',
 			'rasgo_id' => $rango2->id,
 			]);
 		Item::create([
 			'name' => 'Valorar el cuidado de la alimentación y cuidado de la salud',
-			'img' => 'salud/item2-4.jpg',
+			'img' => 'salud/2-4.png',
 			'rasgo_id' => $rango2->id,
 			]);
 		Item::create([
 			'name' => 'Practicar regularmente una disciplina deportiva',
-			'img' => 'salud/item2-5.jpg',
+			'img' => 'salud/2-5.png',
 			'rasgo_id' => $rango2->id,
 			]);
 		Item::create([
 			'name' => 'Realizar actividades tranquilas al aire libre en contacto con la naturaleza',
-			'img' => 'salud/item2-6.jpg',
+			'img' => 'salud/2-6.png',
 			'rasgo_id' => $rango2->id,
 			]);
 		Item::create([
 			'name' => 'Realizar viajes o exploraciones riesgosas en diferentes regiones',
-			'img' => 'salud/item2-7.jpg',
+			'img' => 'salud/2-7.png',
 			'rasgo_id' => $rango2->id,
 			]);
 		
 		Item::create([
 			'name' => 'Reparar, armar o desarmar cosas',
-			'img' => 'tecnologia/item3-1.jpg',
+			'img' => 'tecnologia/3-1.png',
 			'rasgo_id' => $rango3->id,
 			]);
 		Item::create([
 			'name' => 'Construir edificios, casas, puentes',
-			'img' => 'tecnologia/item3-2.jpg',
+			'img' => 'tecnologia/3-2.png',
 			'rasgo_id' => $rango3->id,
 			]);
 		Item::create([
 			'name' => 'Cuidar un jardín, cultivar y cosechar.',
-			'img' => 'tecnologia/item3-3.jpg',
+			'img' => 'tecnologia/3-3.png',
 			'rasgo_id' => $rango3->id,
 			]);
 		Item::create([
 			'name' => 'Realizar actividades relacionadas al ciclo sustentable del planeta, reciclaje',
-			'img' => 'tecnologia/item3-4.jpg',
+			'img' => 'tecnologia/3-4.png',
 			'rasgo_id' => $rango3->id,
 			]);
 		Item::create([
 			'name' => 'Crear y diseñar productos de diverso tipo',
-			'img' => 'tecnologia/item3-5.jpg',
+			'img' => 'tecnologia/3-5.png',
 			'rasgo_id' => $rango3->id,
 			]);
 		Item::create([
 			'name' => 'Investigar nuevas formas de comunicación tecnológica',
-			'img' => 'tecnologia/item-3-6.jpg',
+			'img' => 'tecnologia/3-6.png',
 			'rasgo_id' => $rango3->id,
 			]);
 
 		Item::create([
 			'name' => 'Escribir poesía, novelas, cuentos, blogs, canciones.',
-			'img' => 'arte/item4-1.jpg',
+			'img' => 'arte/4-1.png',
 			'rasgo_id' => $rango4->id,
 			]);
 		Item::create([
 			'name' => 'Ejecutar instrumentos',
-			'img' => 'arte/item4-2.jpg',
+			'img' => 'arte/4-2.png',
 			'rasgo_id' => $rango4->id,
 			]);
 		Item::create([
 			'name' => 'Bailar, cantar, actuar',
-			'img' => 'arte/item4-3.jpg',
+			'img' => 'arte/4-3.png',
 			'rasgo_id' => $rango4->id,
 			]);
 		Item::create([
 			'name' => 'Dibujar, pintar, fotografiar',
-			'img' => 'arte/item4-4.jpg',
+			'img' => 'arte/4-4.png',
 			'rasgo_id' => $rango4->id,
 			]);
 		Item::create([
 			'name' => 'Desarrollar actividades manuales (modelado de objetos, esculturas, tejidos, origami) ',
-			'img' => 'arte/item4-5.jpg',
+			'img' => 'arte/4-5.png',
 			'rasgo_id' => $rango4->id,
 			]);
 		Item::create([
 			'name' => 'Crear nuevas formas de expresar belleza y armonía',
-			'img' => 'arte/item4-6.jpg',
+			'img' => 'arte/4-6.png',
 			'rasgo_id' => $rango4->id,
 			]);
 		Item::create([
 			'name' => 'Comprar y vender cosas para tener una ganancia',
-			'img' => 'economia/item5-1.jpg',
+			'img' => 'economia/5-1.png',
 			'rasgo_id' => $rango5->id,
 			]);
 		Item::create([
 			'name' => 'Pensar en nuevas cosas que las personas necesiten consumir ',
-			'img' => 'economia/item5-2.jpg',
+			'img' => 'economia/5-2.png',
 			'rasgo_id' => $rango5->id,
 			]);
 		Item::create([
 			'name' => 'Hacer cosas nuevas siempre, crear, inventar servicios',
-			'img' => 'economia/item5-3.jpg',
+			'img' => 'economia/5-3.png',
 			'rasgo_id' => $rango5->id,
 			]);
 
 		Item::create([
 			'name' => 'Dirigir un grupo de personas para lograr un resultado',
-			'img' => 'economia/item5-4.jpg',
+			'img' => 'economia/5-4.png',
 			'rasgo_id' => $rango5->id,
 			]);
 		Item::create([
 			'name' => 'Definir estrategias para satisfacer a Clientes',
-			'img' => 'economia/item5-5.jpg',
+			'img' => 'economia/5-5.png',
 			'rasgo_id' => $rango5->id,
 			]);
 		Item::create([
 			'name' => 'Entender por qué y cómo funciona la economía del país y el mundo',
-			'img' => 'economia/item5-6.jpg',
+			'img' => 'economia/5-6.png',
 			'rasgo_id' => $rango5->id,
 			]);
 
--- public/js/survey.surveyItem.directive.js
+++ public/js/survey.surveyItem.directive.js
@@ -3,14 +3,14 @@
     .directive('surveyItem', function(){
       return {
         restrict: 'C',
-        template: '<div style="cursor: pointer"' +
+        template: '<div style="cursor: pointer; height: 430px"' +
         ' class="centrado pregunta" ng-class="{active: item.selected}" '+
           'onclick="$(this).find(\'input:checkbox\').click()">' +
-          '<img class="img-responsive center-block" width="205px" style="height: 140px" ng-src="/img/items/{{ item.img }}"' +
+          '<img class="img-responsive center-block" width="205px" style="height: 200px" ng-src="/img/items/{{ item.img }}"' +
           ' alt="{{ item.name }}" >' +
-          '<p>{{ item.name }}</p>' +
+          '<p style="height: 130px;">{{ item.name }}</p>' +
           '<div class="checkbox-item">' +
-            	'<input type="checkbox"  style="cursor: inherit" ng-model="item.selected" onclick="event.stopPropagation()" value="None"' +
+            	'<input type="checkbox"  style="cursor: inherit; bottom: 0" ng-model="item.selected" onclick="event.stopPropagation()" value="None"' +
                ' id="selected" name="selected" />' +
           '</div>' +
         '</div>',
