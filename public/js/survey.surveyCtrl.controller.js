bfa274faca7bf4e88f56e5946f62bb793d71b9ea
rodrigo valdez <rodri.valdez@gmail.com>
2015-07-06 16:00:54 -0400

    Agregar Descripcion

--- public/js/survey.surveyCtrl.controller.js
+++ public/js/survey.surveyCtrl.controller.js
@@ -16,25 +16,22 @@
         'Terminamos :)'
         ];
 
-      $controller.felicidad = $sce.trustAs('html',
-      '<p>Este cuestionario fue creado para que la persona que lo realice pueda identificar alguna de sus motivaciones o intereses profesionales enmarcados o clasificados en cinco campos determinados por las diferentes profesiones más conocidas.
-Estos campos profesionales son: </p>
-
-1-  Social <br>  
-2-  Salud-deporte <br>
-3-  Artístico <br>
-4-  Tecnológico <br>
-5-  Negocios<br> 
-<p>Sabemos que podrían existir otros campos profesionales pero el objetivo actual es identificar la motivación inicial para una posterior búsqueda más profunda con mayores elementos personales que se deben tener para una elección profesional.
-METODOLOGIA</p>
-
-1-  Lee y contesta con tu primer impulso todas las frases que aparecen a continuación
-2-  Deberás elegir la opción me gusta para contestarlas por lo tanto las que no me gustan ya no se tendrán en cuenta en la puntuación final <br>
-3-  Cuando contestes todo, podrás esperar inmediatamente un resultado de acuerdo a lo que has elegido (o puntuado). <br>
-4-  Las conclusiones finales serán las que tuvieron mayor puntuación según la estructura  interna del cuestionario y lo que la persona ha puntuado. <br>
-5-  Después de leer tu resultado debes anotar si te pareció adecuado y te sentiste identificado con el (el resultado) <br>
-6-  Podrás dejar tus datos si quisieras que te contactemos para un mejor análisis de tus intereses profesionales. <br>
-');
+      $controller.felicidad = $sce.trustAs('html','<p>Este cuestionario fue creado para que la persona que lo realice pueda identificar'+
+       'alguna de sus motivaciones o intereses profesionales enmarcados o clasificados en cinco campos determinados por las diferentes profesiones más conocidas.'+
++'Estos campos profesionales son: </p>'+
+'1-  Social <br>  '+
+'2-  Salud-deporte <br>'+
+'3-  Artístico <br>' +
+'4-  Tecnológico <br>' +
+'5-  Negocios<br> '+
+'<p>Sabemos que podrían existir otros campos profesionales pero el objetivo actual es identificar la motivación inicial para una posterior búsqueda más profunda con mayores elementos personales que se deben tener para una elección profesional.METODOLOGIA</p>'+
+'1-  Lee y contesta con tu primer impulso todas las frases que aparecen a continuación'+
+'2-  Deberás elegir la opción me gusta para contestarlas por lo tanto las que no me gustan ya no se tendrán en cuenta en la puntuación final <br>'+
+'3-  Cuando contestes todo, podrás esperar inmediatamente un resultado de acuerdo a lo que has elegido (o puntuado). <br>'+
+'4-  Las conclusiones finales serán las que tuvieron mayor puntuación según la estructura  interna del cuestionario y lo que la persona ha puntuado. <br>'+
+'5-  Después de leer tu resultado debes anotar si te pareció adecuado y te sentiste identificado con el (el resultado) <br>'+
+'6-  Podrás dejar tus datos si quisieras que te contactemos para un mejor análisis de tus intereses profesionales. <br>'
+);
 
       $controller.retest = {
         "title": "Se necesita rehacer.",
